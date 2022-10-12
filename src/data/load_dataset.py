129c0f85c02ddb3d80f0b3abc60a1dd37151c8f8
Max-jones <maximilianojones@gmail.com>
2022-10-12 12:48:52 -0300

    cambios

--- src/data/load_dataset.py
+++ src/data/load_dataset.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import pytz
 
 
 def load_data(path):
@@ -9,19 +10,33 @@ def load_data(path):
     '''
 
     data = pd.read_csv(path, sep=None, engine='python',encoding = 'utf-8-sig',parse_dates= True)
+    data.dropna(axis="columns", how="any", inplace=True)
     try:
 
-        data["Date_Time"] = pd.to_datetime(data["Date_Time"])
+        data["Date_Time"] = pd.to_datetime(data["Date_Time"],dayfirst=True)
         data.set_index("Date_Time", inplace=True)
         chile = pytz.timezone("Chile/Continental")
         data.index = data.index.tz_localize(pytz.utc).tz_convert(chile)
+        print('1')
         return data
     except:
         try:
-            data['Date_Time'] = pd.to_datetime(data["Date_Time"])
+            data['Date_Time'] = pd.to_datetime(data["Date_Time"],dayfirst=True)
             data.set_index("Date_Time", inplace=True)
             chile = pytz.timezone("Chile/Continental")
             data.index = data.index.tz_localize(pytz.utc).tz_convert(chile)
+            print('2')
             return data
         except:
-            return data
+            try: 
+                data['Date_Time'] = data['Date'] + ' ' + data['Time']
+                data.drop(['Date','Time'],axis=1,inplace=True)
+                data['Date_Time'] = pd.to_datetime(data["Date_Time"],dayfirst=True)
+                data.set_index("Date_Time", inplace=True)
+                chile = pytz.timezone("Chile/Continental")
+                data.index = data.index.tz_localize(pytz.utc).tz_convert(chile)
+                print('3')
+                return data
+            except:
+                print('4')
+                return data
